bug fix to splice averaging procedure

tools/MaskScorer/MaskScorer.py
@@ -555,13 +555,14 @@ elif args.task == 'splice':
         journalData0.to_csv(path_or_buf=os.path.join(outRootQuery,prefix + '-journalResults.csv'),index=False)
         a_df = 0
     
-        r_df.loc[r_df.query('pMCC == -2').index,'pNMM'] = ''
-        r_df.loc[r_df.query('pMCC == -2').index,'pBWL1'] = ''
-        r_df.loc[r_df.query('pMCC == -2').index,'pGWL1'] = ''
-        r_df.loc[r_df.query('dMCC == -2').index,'dNMM'] = ''
-        r_df.loc[r_df.query('dMCC == -2').index,'dBWL1'] = ''
-        r_df.loc[r_df.query('dMCC == -2').index,'dGWL1'] = ''
-
+        p_idx = r_df.query('pMCC == -2').index
+        d_idx = r_df.query('dMCC == -2').index
+        r_df.loc[p_idx,'pNMM'] = ''
+        r_df.loc[p_idx,'pBWL1'] = ''
+        r_df.loc[p_idx,'pGWL1'] = ''
+        r_df.loc[d_idx,'dNMM'] = ''
+        r_df.loc[d_idx,'dBWL1'] = ''
+        r_df.loc[d_idx,'dGWL1'] = ''
         #reorder r_df's columns. Names first, then scores, then other metadata
         rcols = r_df.columns.tolist()
         firstcols = ['TaskID','ProbeFileID','ProbeFileName','ProbeMaskFileName','DonorFileID','DonorFileName','DonorMaskFileName','IsTarget','OutputProbeMaskFileName','OutputDonorMaskFileName','ConfidenceScore','pNMM','pMCC','pBWL1','pGWL1','dNMM','dMCC','dBWL1','dGWL1']
@@ -571,11 +572,20 @@ elif args.task == 'splice':
     
         metrics = ['pNMM','pMCC','pBWL1','pGWL1','dNMM','dMCC','dBWL1','dGWL1']
         r_dfc = r_df.copy()
-        r_dfc.loc[r_dfc.query('pMCC == -2').index,'pMCC'] = ''
         r_dfc.loc[r_dfc.query('pMCC == -2').index,'ProbeScored'] = 'N'
-        r_dfc.loc[r_dfc.query('dMCC == -2').index,'dMCC'] = ''
         r_dfc.loc[r_dfc.query('dMCC == -2').index,'DonorScored'] = 'N'
-        my_partition = pt.Partition(r_dfc,query,factor_mode,metrics) #average over queries #TODO: move this into the query section a'la manipulation
+        p_dummyscores = r_dfc.query("pMCC > -2").mean(axis=0)
+        d_dummyscores = r_dfc.query("dMCC > -2").mean(axis=0)
+
+        r_dfc.loc[p_idx,'pNMM'] = p_dummyscores['pNMM']
+        r_dfc.loc[p_idx,'pBWL1'] = p_dummyscores['pBWL1']
+        r_dfc.loc[p_idx,'pGWL1'] = p_dummyscores['pGWL1']
+        r_dfc.loc[d_idx,'dNMM'] = d_dummyscores['dNMM']
+        r_dfc.loc[d_idx,'dBWL1'] = d_dummyscores['dBWL1']
+        r_dfc.loc[d_idx,'dGWL1'] = d_dummyscores['dGWL1']
+        r_dfc.loc[p_idx,'pMCC'] = p_dummyscores['pMCC']
+        r_dfc.loc[d_idx,'dMCC'] = d_dummyscores['dMCC']
+        my_partition = pt.Partition(r_dfc,query,factor_mode,metrics) #average over queries.
         df_list = my_partition.render_table(metrics)
     
         if args.query and (len(df_list) > 0): #don't print anything if there's nothing to print