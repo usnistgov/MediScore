36e67125a4dcdebf701a14cacacb82d2bc5fe222
dfz <daniel.zhou@nist.gov>
2017-05-02 16:05:00 -0400

    fixed partition bug

--- lib/Partition_mask.py
+++ lib/Partition_mask.py
@@ -242,7 +242,7 @@ class Partition:
             dm = self.part_metric_list[0]
             for m in metrics:
                 #set all '' to np.nan
-                dm.loc[dm[m]=='',]=np.nan
+                dm.loc[dm[m]=='',m]=np.nan
                 data[m] = [dm[m].mean(skipna=True)]
             data['TaskID'] = [self.task]
             data['Query'] = self.part_query_list
