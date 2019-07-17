003e926dc54716b6b28e2bdfb7c9d1bc68c70c1b
tkheyrkhah <timothee.kheyrkhah@nist.gov>
2019-07-17 13:10:52 -0400

    Fixed datacontainer handling with empty array inputs and missing values

--- tools/DetectionScorer/datacontainer.py
+++ tools/DetectionScorer/datacontainer.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 import sys
 import pickle
+import numpy as np
 from collections import OrderedDict
 
 class DataContainer:
@@ -14,16 +15,41 @@ class DataContainer:
         self.fa_label = fa_label
         self.fn_label = fn_label
 
+        self.validate_array_input()
+
         if line_options is not None:
             self.line_options = line_options
             # Assuring consistency between the label attribute and the line label property
-            if ("label" not in self.line_options) and (self.label is not None)
+            if ("label" not in self.line_options) and (self.label is not None):
                 self.line_options["label"] = self.label
-            elif ("label" in self.line_options) and (self.label is None)
+            elif ("label" in self.line_options) and (self.label is None):
                 self.label = self.line_options["label"]
         else:
             self.line_options = DataContainer.get_default_line_options()
             self.line_options["label"] = label
+
+    def validate_array_input(self):
+        # Checking array type, dimensions and value types
+        for array, (arg_name, attr_name) in zip([self.fa, self.fn, self.threshold], 
+                                                [["fa_array", "fa"], ["fn_array", "fn"], ["threshold", "threshold"]]):
+            if not isinstance(array, np.ndarray):
+                print("Error: '{}'' must be of type 'numpy.ndarray'".format(arg_name))
+                sys.exit(1)
+
+            if array.ndim != 1:
+                print("Error: '{}' must be one dimensionnal".format(arg_name))
+                sys.exit(1)
+
+            if array.dtype == "O": # converting potential None values to np.nan first
+                new_array = np.array([np.nan if x is None else x for x in array])
+                if np.issubdtype(new_array.dtype, np.number):
+                    setattr(self, attr_name, new_array)
+                else:
+                    print("Error: '{}' must be numeric ({})".format(arg_name, new_array.dtype))
+                    sys.exit(1)
+            elif not np.issubdtype(array.dtype, np.number):
+                print("Error: '{}' must be numeric ({})".format(arg_name, array.dtype))
+                sys.exit(1)
 
     def set_default_line_options(self):
         self.line_options = DataContainer.get_default_line_options()
@@ -38,15 +64,15 @@ class DataContainer:
 
     @staticmethod
     def load(path):
-    """ Load Dumped files
-        path: absolute path to the file
-    """
-    with open(path, 'rb') as file:
-        if sys.version_info[0] >= 3:
-            obj = pickle.load(file, encoding='latin1') 
-        else:
-            obj = pickle.load(file)
-    return obj
+        """ Load Dumped files
+            path: absolute path to the file
+        """
+        with open(path, 'rb') as file:
+            if sys.version_info[0] >= 3:
+                obj = pickle.load(file, encoding='latin1') 
+            else:
+                obj = pickle.load(file)
+        return obj
 
     @staticmethod
     def get_default_line_options():
@@ -60,16 +86,18 @@ class DataContainer:
                             ('label', None),
                             ('antialiased', 'False')])
 
-    
-
     @staticmethod
     def aggregate(dc_list, output_label="Average", method="average", average_resolution=500, line_options=None):
+        # Filtering data with missing value
+        is_valid = lambda dc: dc.fa.size != 0 and dc.fn.size != 0 and np.all(~np.isnan(dc.fa)) and np.all(~np.isnan(dc.fb))
+        dc_list_filtered = [dc for dc in dc_list if is_valid(dc)]
+        
+        if dc_list_filtered:
+            if line_options is None:
+                default_line_options = DataContainer.get_default_line_options()
+                default_line_options["color"] = "green"
 
-        if line_options is None:
-            default_line_options = DataContainer.get_default_line_options()
-            default_line_options["color"] = "green"
-
-        if method == "average":
-            x = np.linspace(0, 1, average_resolution)
-            ys = [np.interp(x, data.fa, data.fn) for data in dc_list]
-            return DataContainer(x, np.vstack(ys).mean(0), label="output_label", line_options=line_options)
+            if method == "average":
+                x = np.linspace(0, 1, average_resolution)
+                ys = [np.interp(x, data.fa, data.fn) for data in dc_list_filtered]
+                return DataContainer(x, np.vstack(ys).mean(0), label="output_label", line_options=line_options)
