e2e7594711537eb60b5f77d253ea7e3bd6948faf
tkheyrkhah <timothee.kheyrkhah@nist.gov>
2019-07-18 16:26:58 -0400

    Updated the new version of the DetMetric object

--- tools/DetectionScorer/medifor_datacontainer.py
+++ tools/DetectionScorer/medifor_datacontainer.py
@@ -33,14 +33,71 @@ class MediForDataContainer(DataContainer):
         self.bpoint = None
         self.tpr_at_fpr = None
         self.ci_level = None
-        self.auc_ci_lower = 0
-        self.auc_ci_upper = 0
-        self.auc_at_fpr_ci_lower = 0
-        self.auc_at_fpr_ci_upper = 0
-        self.tpr_at_fpr_ci_lower = 0
-        self.tpr_at_fpr_ci_upper = 0
+        self.auc_ci_lower = None
+        self.auc_ci_upper = None
+        self.auc_at_fpr_ci_lower = None
+        self.auc_at_fpr_ci_upper = None
+        self.tpr_at_fpr_ci_lower = None
+        self.tpr_at_fpr_ci_upper = None
         self.fpr_stop = None
         self.sys_res = None
+
+    def set_groundtruth(self, groundtruth):
+        self.gt = groundtruth
+        print("Attributes set: 'gt'")
+
+    def set_scores(self, scores):
+        self.scores = scores
+        print("Attributes set: 'scores'")
+
+    def set_target_stats(self, target_label="Y", non_target_label="N"):
+        print("Computing the number of target and non-target based on the provided ground truth labels")
+        if self.gt is not None:
+            self.t_num = np.count_nonzero(gt == target_label)
+            self.nt_num = np.count_nonzero(gt == non_target_label)
+            print("Attributes set: 't_num', 'nt_num'")
+        else:
+            print("Error [set_target_stats]: Missing attributes.\nPlease set the ground truth array by calling set_groundtruth() before.")
+    
+    def set_trial_reponse_rate(self, total_trial):
+        if (self.t_num is None or self.nt_num is None):
+            print("Error [set_trial_response_rate]: Missing attributes.\nPlease set the target statistics by using the method set_target_stats() before calling this method.")
+        else:
+            if total_trial != 0:
+                self.trr = round((self.t_num + self.nt_num) / total_trial, 2)
+                print("Attributes set: 'trr")
+            else:
+                print("Error: the total number of trial without optout must be different from 0")
+
+    def set_eer(self):
+        self.eer = Metrics.compute_eer(self.fa, self.fn)
+        print("Attributes set: 'eer'")
+
+    def set_auc(self):
+        self.auc = Metrics.compute_auc(self.fa, 1 - self.fn, fpr_stop=1)
+        print("Attributes set: 'auc'")
+
+    def set_auc_at_fpr(self, fpr_stop=1):
+        self.fpr_stop = fpr_stop
+        self.auc_at_fpr = Metrics.compute_auc(self.fa, 1 - self.fn, fpr_stop=fpr_stop)
+        print("Attributes set: 'auc_at_fpr', 'fpr_stop'")
+
+    def set_dprime(self, dLevel=0.0):
+        self.d, self.dpoint, self.b, self.bpoint = Metrics.compute_dprime(self.fa, self.tpr, d_level=dLevel)
+        print("Attributes set: 'd', 'dpoint', 'b', 'bpoint'")
+
+    def set_sys_res(self, sys_res):
+        self.sys_res = sys_res
+        print("Attributs set: 'sys_res'")
+
+    def set_ci(self, ciLevel=0.9, fpr_stop=1):
+        if self.scores is not None:
+            self.ci_level = ciLevel
+            self.fpr_stop = fpr_stop
+            self.auc_ci_lower, self.auc_ci_upper, self.auc_at_fpr_ci_lower, self.auc_at_fpr_ci_upper, self.tpr_at_fpr_ci_lower, self.tpr_at_fpr_ci_upper = Metrics.compute_ci(score, gt, ciLevel, fpr_stop)
+            print("Attributes set: 'ci_level', 'fpr_stop', 'auc_ci_lower', 'auc_ci_upper', 'auc_at_fpr_ci_lower', 'auc_at_fpr_ci_upper', 'tpr_at_fpr_ci_lower', 'tpr_at_fpr_ci_upper'")
+        else:
+            print("Error [set_ci]: Missing attributes.\nPlease set the scores array by calling the method set_scores() before.")
 
     def __repr__(self):
         """Print from interpretor"""
