c6378b35225f03bb1996e549286f9093974de8f2
tkheyrkhah <timothee.kheyrkhah@nist.gov>
2019-07-19 16:30:47 -0400

    Fixed DET plot y scale issue

--- tools/DetectionScorer/render.py
+++ tools/DetectionScorer/render.py
@@ -53,13 +53,16 @@ class Render:
             return fig
 
 
-    def plotter(self, data_list, annotations, plot_type, plot_options, display):
+    def plotter(self, data_list, annotations, plot_type, plot_options, display, infinity=999999):
         fig = plt.figure(figsize=plot_options["figsize"], dpi=120, facecolor='w', edgecolor='k')
 
-        get_y = lambda fn, plot_type: fn if plot_type == "det" else 1 - fn
-        
+        get_y = lambda fn, plot_type: norm.ppf(fn) if plot_type == "det" else 1 - fn
+
         for obj in data_list:
-            plt.plot(obj.fa, get_y(obj.fn, plot_type), **obj.line_options)
+            x = obj.fa
+            y = get_y(obj.fn, plot_type)
+            y[y == np.inf] = infinity
+            plt.plot(x, y, **obj.line_options)
 
         # if plot_type.lower() == "det":
         #     plt.plot((1, 0), 'b--', lw=1)
@@ -72,6 +75,7 @@ class Render:
 
         plt.xlim(plot_options["xlim"])
         plt.ylim(plot_options["ylim"])
+        print("ylim = {}".format(plot_options["ylim"]))
         plt.xlabel(plot_options['xlabel'], fontsize=plot_options['xlabel_fontsize'])
         plt.ylabel(plot_options['ylabel'], fontsize=plot_options['ylabel_fontsize'])
         plt.xscale(plot_options["xscale"])
@@ -125,12 +129,12 @@ class Render:
             plot_opts["ylabel"] = "Miss Detection Rate [%]"
             # plot_opts["xticks"] = norm.ppf([.0001, 0.0002, 0.0005, 0.001, 0.002, 0.005, .01, .02, .05, .10, .20, .40, .60, .80, .90, .95, .98, .99, .995, .999])
             plot_opts["xticks"] = [0.01, 0.1, 1, 10]
-            plot_opts["yticks"] = norm.ppf([.0001, 0.0002, 0.0005, 0.001, 0.002, 0.005, .01, .02, .05, .10, .20, .40, .60, .80, .90, .95, .98, .99, .995, .999])
+            plot_opts["yticks"] = norm.ppf([0.05, 0.1, 0.2, 0.4, 0.6, 0.8, 0.9, 0.95, 0.98, 0.99, 0.995, 0.999])
             plot_opts["xlim"] = (plot_opts["xticks"][0], plot_opts["xticks"][-1])
             plot_opts["ylim"] = (plot_opts["yticks"][0], plot_opts["yticks"][-1])
             # plot_opts["xticks_labels"] = ['0.01', '0.02', '0.05', '0.1', '0.2', '0.5', '1', '2', '5', '10', '20', '40', '60', '80', '90', '95', '98', '99', '99.5', '99.9']
             plot_opts["xticks_labels"] = ["0.01", "0.1", "1", "10"]
-            plot_opts["yticks_labels"] = ['0.01', '0.02', '0.05', '0.1', '0.2', '0.5', '1', '2', '5', '10', '20', '40', '60', '80', '90', '95', '98', '99', '99.5', '99.9']
+            plot_opts["yticks_labels"] = ['5.0', '10.0', '20.0', '40.0', '60.0', '80.0', '90.0', '95.0', '98.0', '99.0', '99.5', '99.9']
 
         elif plot_type.lower() == "roc":
             plot_opts["xscale"] = "linear"
@@ -141,8 +145,6 @@ class Render:
             plot_opts["xticks_labels"] = ['0', '10', '20', '30', '40', '50', '60', '70', '80', '90', '100']
 
         return plot_opts
-
-    
 
 class Annotation:
     def __init__(self, text, parameters):
