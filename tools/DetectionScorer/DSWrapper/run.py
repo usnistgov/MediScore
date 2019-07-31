054226ed50bb3266e7b633c749fcf7947df143e3
tkheyrkhah <timothee.kheyrkhah@nist.gov>
2019-07-31 16:35:24 -0400

    Change usage of system call

--- tools/DetectionScorer/DSWrapper/run.py
+++ tools/DetectionScorer/DSWrapper/run.py
@@ -3,7 +3,7 @@ import sys
 import shlex
 import subprocess
 
-command_template = "python {script_path} --sysDir {sysDir} --refDir {refDir} -s {system} -x {index} -r {ref} -o {output} {verbose} {options}"
+command_template = "python {script_path} --sysDir {sysDir} --refDir {refDir} -s {system} -x {index} -r {ref} -o {output} {verbose} {options} 1> f.stdout 2> f.stderr"
 
 script_path = "../DetectionScorer.py"
 sysDir = "/Users/tnk12/Documents/MediScoreV2/tools/DetectionScorer/DSWrapper/system/"
@@ -22,9 +22,9 @@ for ss in ss_dicts:
     cmd = command_template.format(script_path=script_path, sysDir=sysDir, refDir=refDir, system=system, index=index, ref=ref, output=output, verbose='-v', options=ss["options"])
     with open("command_x.sh",'w') as f:
         f.write(cmd)
-    print(shlex.split(cmd))
-    p = subprocess.run(shlex.split(cmd), capture_output=True)
-    print(p.stdout.decode())
-    print(p.stderr.decode())
+    # p = subprocess.run(shlex.split(cmd), capture_output=True)
+    # print(p.stdout.decode())
+    # print(p.stderr.decode())
+    os.system(cmd)
 
 
